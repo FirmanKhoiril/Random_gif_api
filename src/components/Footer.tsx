0c6da242f3d73ce50c92da53c39774640b3a0a17
firmankhoiril <firmankhoiril13@gmail.com>
2023-01-14 13:41:36 +0700

    change color footer h3

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
     <div className="w-full mt-4 bg-white/10 p-3">
       <h2>
         @Created By{" "}
-        <a href={Instagram} className=" bg-gradient-to-r bg-clip-text text-transparent font-semibold from-lime-500 to-sky-400" target="_blank">
+        <a href={Instagram} className=" bg-gradient-to-r bg-clip-text text-transparent font-semibold from-lime-500 to-green-400" target="_blank">
           Firman Khoiril
         </a>
       </h2>
